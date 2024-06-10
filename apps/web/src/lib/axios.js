f14dd393c8f38a14ab8da1ca015950b9d6da2362
mesakh0505 <mesakhhartono@gmail.com>
2024-06-10 17:06:10 +0700

    feat: frontendCart

---
+++ apps/web/src/lib/axios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+function ApiInstance(
+	authorization = null,
+	password = null,
+	confirmPassword = null,
+	tokentype = null
+) {
+	return axios.create({
+		baseURL: process.env.REACT_APP_API_BASE_URL,
+		headers: {
+			authorization: `Bearer ${authorization}`,
+			password,
+			confirmPassword,
+			tokentype,
+		},
+	});
+}
+
+export const axiosInstance = ApiInstance;
